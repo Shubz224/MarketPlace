b3514ae258389ddfb6036ce515707b9944a7aef1
Shubham <nikamshubham224@gmail.com>
2024-07-04 23:48:18 -0700

    Added lazy loading for pages

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,12 +1,24 @@
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import {Suspense, lazy} from 'react'
+import Loader from './Components/loader';
+const  Cart = lazy(()=>import('./Pages/cart')) ;
+const  Home = lazy(()=>import('./Pages/home')) 
+const  Search = lazy(()=>import('./Pages/search'));
 
- const App = () => {
+const App = () => {
   return (
-    <div> <h1>
-      Hey there !
-    </h1>
-    </div>
+    <Router>
+      {/*Header */}
+       <Suspense fallback ={<Loader/>}>
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+       </Suspense>
+    </Router>
   )
 }
 export default App;
---
+++ ecommerce-frontend/src/Components/loader.tsx
@@ -0,0 +1,8 @@
+
+const Loader = () => {
+  return (
+    <div>Loading </div>
+  )
+}
+
+export default Loader

a7da0dbab7f66f6a35afd303ebba2065bfdd196a
Shubham <nikamshubham224@gmail.com>
2024-07-07 02:43:34 -0700

    added routes for admin dashboard

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -38,6 +38,33 @@ const App = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/search" element={<Search />} />
 
+        //
+        <Route
+  // element={
+  //   <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
+  // }
+>
+  <Route path="/admin/dashboard" element={<Dashboard />} />
+  <Route path="/admin/product" element={<Products />} />
+  <Route path="/admin/customer" element={<Customers />} />
+  <Route path="/admin/transaction" element={<Transaction />} />
+  {/* Charts */}
+  <Route path="/admin/chart/bar" element={<Barcharts />} />
+  <Route path="/admin/chart/pie" element={<Piecharts />} />
+  <Route path="/admin/chart/line" element={<Linecharts />} />
+  {/* Apps */}
+  <Route path="/admin/app/coupon" element={<Coupon />} />
+  <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+  <Route path="/admin/app/toss" element={<Toss />} />
+
+  {/* Management */}
+  <Route path="/admin/product/new" element={<NewProduct />} />
+
+  <Route path="/admin/product/:id" element={<ProductManagement />} />
+
+  <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
+</Route>;
+
       </Routes>
        </Suspense>
     </Router>

00d0c6cb426fa782cd86d1346cb9b26438c9d7e2
Shubham <nikamshubham224@gmail.com>
2024-07-12 12:34:36 +0530

    Added dialog box for user and admin profiles

--- ecommerce-frontend/src/Components/header.tsx
+++ ecommerce-frontend/src/Components/header.tsx
@@ -1,21 +1,43 @@
 
 import { Link } from "react-router-dom"
-import { FaSearch, FaShoppingBag, FaSignInAlt, FaUser } from "react-icons/fa"
+import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa"
+import { useState } from "react";
 
-const user = { _id: "" };
+const user = { _id: " ", role: "admin" };
 
 const Header = () => {
+
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+
   return (
 
-    <nav>
+    <nav className="header">
       <Link to={"/"}>home</Link>
       <Link to={"/search"}><FaSearch /></Link>
 
       <Link to={"/cart"}><FaShoppingBag /></Link>
       {
-        user?._id? (
+        user?._id ? (
           <>
-          <FaUser/>
+            <button onClick={() => setIsOpen(prev => !prev)}>
+              <FaUser />
+            </button>
+            <dialog open={isOpen}>
+              <div>
+                {
+                  user.role === "admin" && (
+                    <Link to="/admin/dashboard">Admin</Link>
+
+                  )
+                }
+
+                <Link to="/orders">Orders</Link>
+                <button>
+                  <FaSignOutAlt />
+                </button>
+              </div>
+            </dialog>
           </>
         ) : <Link to={"/login"}><FaSignInAlt /></Link>
       }

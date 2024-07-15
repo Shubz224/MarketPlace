929409280e8a042da5d28261410efd9a4ac4879a
Shubham <nikamshubham224@gmail.com>
2024-07-15 14:39:04 +0530

    Added product card to front home page

--- ecommerce-frontend/src/Pages/home.tsx
+++ ecommerce-frontend/src/Pages/home.tsx
@@ -1,17 +1,24 @@
 
 import { Link } from "react-router-dom"
-
+import ProductCard from "../Components/product-card";
 const Home = () => {
+
+//temporary handler 
+
+const addTocartHandler =()=>{
+
+}
   return (
     <div className="home">
     <section></section>
-
 
     <h1>
      Latest Products
      <Link to = "/search" className="findmore"> More</Link>
     </h1>
-    <main></main>
+    <main>
+    <ProductCard  photo="https://m.media-amazon.com/images/I/71xlzzUl6aL._AC_UL320_.jpg" productId="dsadasd"  name="nike"  price={45454} stock={43} handler={addTocartHandler}/>
+    </main>
 </div>
   );
 };

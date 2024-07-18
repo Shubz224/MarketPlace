b35624d173ebe8f242a4e1807d4b00f3406b58e9
Shubham <nikamshubham224@gmail.com>
2024-07-18 18:54:34 +0530

    .

--- ecommerce-frontend/src/Components/header.tsx
+++ ecommerce-frontend/src/Components/header.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa"
 import { useState } from "react";
 
+// user added then login page disappeard bug .....
 const user = { _id: "", role: "user" };
 
 const Header = () => {

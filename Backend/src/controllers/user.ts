515cfb3b1c3f6a78bcddfa591bda5c2d1bf4efd3
Shubham Nikam <nikamshubham224@gmail.com>
2024-07-25 13:24:25 +0530

    wrapped newUser Function around TryCatch Error Handler

--- Backend/src/controllers/user.ts
+++ Backend/src/controllers/user.ts
@@ -1,33 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 import { User } from "../models/user.js";
 import { NewUserrequestBody } from "../types/types.js";
+import { TryCatch } from "../middlewares/error.js";
 
-export const newUser = async (
-    req:Request<{}, {}, NewUserrequestBody>, 
-    res: Response, 
-    next: NextFunction) => {
-
-    try {
-
-        const { name, email, photo, gender, _id, dob } = req.body;
-     
-       
-        console.log( name, email, photo, gender, _id, dob);
-      const user =   await User.create({
-            name, email, photo, gender, _id, dob : new Date(dob)
-        });
-       
+export const newUser = TryCatch(
+  
+  async (
+    req: Request<{}, {}, NewUserrequestBody>,
+    res: Response,
+    next: NextFunction
+  ) => {
 
 
-        return res.status(200).json({
-            sucess: true,
-            message: `Welcome,${user.name}`,
-        });
+    const { name, email, photo, gender, _id, dob } = req.body;
 
-    } catch (error) {
-        return res.status(200).json({
-            sucess: false,
-            message: error,
-        })
-    }
-};
+    console.log(name, email, photo, gender, _id, dob);
+    const user = await User.create({
+      name,
+      email,
+      photo,
+      gender,
+      _id,
+      dob: new Date(dob),
+    });
+
+    return res.status(200).json({
+      sucess: true,
+      message: `Welcome,${user.name}`,
+    });
+  }
+);

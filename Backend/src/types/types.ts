f43398422367afd6a50302c10cfe93404093b140
Shubham Nikam <nikamshubham224@gmail.com>
2024-07-25 12:04:34 +0530

    Created tstypes for  function,with types for req and res objects

--- Backend/src/types/types.ts
+++ Backend/src/types/types.ts
@@ -1,9 +1,14 @@
-
-export interface NewUserrequestBody{
-    name:String;
-    email:String;
-    photo:String;
-    gender:String;
-    _id:String;
-    dob:Date;
+import { Request, Response, NextFunction } from "express";
+export interface NewUserrequestBody {
+  name: String;
+  email: String;
+  photo: String;
+  gender: String;
+  _id: String;
+  dob: Date;
 }
+export type ControllerType = (
+  req: Request<{}, {}, NewUserrequestBody>,
+  res: Response,
+  next: NextFunction
+) => Promise<void | Response<any,Record<string, any>>>;

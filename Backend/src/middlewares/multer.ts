327567dd4b2e1ab6bebd49f67c70b4149368a407
Shubham Nikam <nikamshubham224@gmail.com>
2024-07-25 15:53:31 +0530

    Multer setup for product photo upload for fast loading diretly from uploads folder at backend

---
+++ Backend/src/middlewares/multer.ts
@@ -0,0 +1,12 @@
+import multer from 'multer'
+const storage = multer.diskStorage({
+
+    destination(req,file,callback){
+        callback(null,"uploads")
+    },
+    filename(req,file,callback){
+        callback(null,file.originalname);
+    },
+});
+
+export const singleUpload = multer({storage}).single("photo");

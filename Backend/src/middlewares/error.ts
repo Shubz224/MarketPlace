91f243c5ce425ac2305cd83786b3bcc48d506d92
Shubham Nikam <nikamshubham224@gmail.com>
2024-07-25 13:25:01 +0530

    Created Wrapper tag

--- Backend/src/middlewares/error.ts
+++ Backend/src/middlewares/error.ts
@@ -1,22 +1,24 @@
-import { NextFunction ,Request,Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import ErrorHandler from "../utils/utility-class.js";
 import { ControllerType } from "../types/types.js";
 
-export const  errorMiddleware = (
-    err:ErrorHandler,
-    req:Request,
-    res:Response,
-    next:NextFunction)=>{
-    
-    err.message  ||=  "Internal Server Error"
-     err.statusCode  ||= 500;
-    
-    return res.status(err.statusCode).json({
-        success:false,
-        message:err.message,
-    })
-}
+export const errorMiddleware = (
+  err: ErrorHandler,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.message ||= "Internal Server Error";
+  err.statusCode ||= 500;
 
+  return res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
 
 //wrapper tag
-export const TryCatch= (func:ControllerType)=>()=>{}; 
+export const TryCatch = (func: ControllerType) => 
+ (req: Request, res: Response, next: NextFunction) => {
+    return Promise.resolve(func(req, res, next)).catch(next);
+};

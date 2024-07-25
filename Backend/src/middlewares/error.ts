9df2ced1ac69bbaeb7fd86ca89204a94f722fd08
Shubham Nikam <nikamshubham224@gmail.com>
2024-07-25 12:02:56 +0530

    created custom error handler which returns error message whenever called

---
+++ Backend/src/middlewares/error.ts
@@ -0,0 +1,22 @@
+import { NextFunction ,Request,Response} from "express";
+import ErrorHandler from "../utils/utility-class.js";
+import { ControllerType } from "../types/types.js";
+
+export const  errorMiddleware = (
+    err:ErrorHandler,
+    req:Request,
+    res:Response,
+    next:NextFunction)=>{
+    
+    err.message  ||=  "Internal Server Error"
+     err.statusCode  ||= 500;
+    
+    return res.status(err.statusCode).json({
+        success:false,
+        message:err.message,
+    })
+}
+
+
+//wrapper tag
+export const TryCatch= (func:ControllerType)=>()=>{}; 

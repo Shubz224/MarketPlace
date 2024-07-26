25f025d437ddde4e9ac5d808592f939e26649249
Shubham Nikam <nikamshubham224@gmail.com>
2024-07-27 00:29:15 +0530

    Added search request and query types ts

--- Backend/src/types/types.ts
+++ Backend/src/types/types.ts
@@ -1,14 +1,46 @@
 import { Request, Response, NextFunction } from "express";
+
 export interface NewUserrequestBody {
-  name: String;
-  email: String;
-  photo: String;
-  gender: String;
-  _id: String;
+  name: string;
+  email: string;
+  photo: string;
+  gender: string;
+  _id: string;
   dob: Date;
 }
+
+export interface NewProductrequestBody {
+  name: string;
+  category: string;
+  photo: string;
+  price: number;
+  stock: number;
+}
+
+//cutomized it to access id of any time in update producct function but we wot do that w'll remove the types and use default (req) types
+
 export type ControllerType = (
-  req: Request<{}, {}, NewUserrequestBody>,
+  req: Request,
   res: Response,
   next: NextFunction
-) => Promise < void | Response<any,Record<string, any>>>;
+) => Promise<void | Response<any, Record<string, any>>>;
+
+export type SearchRequestQuery = {
+  search?: string;
+  price?: string;
+  category?: string;
+  sort?: string;
+  page?: string;
+};
+
+export interface baseQuerytype {
+  name?: {
+    $regex: string;
+    $options: string;
+  };
+  price?: {
+    $lte: number;
+  };
+
+  category?: string;
+}

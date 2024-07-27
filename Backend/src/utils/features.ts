fed86d4f10f0facdb8476641d92ec4a762ff4f51
Shubham Nikam <nikamshubham224@gmail.com>
2024-07-27 14:24:08 +0530

    Added invalidadtesCache function to remove cached items from client side when new products are updated or deleted from db

--- Backend/src/utils/features.ts
+++ Backend/src/utils/features.ts
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import { invalidadtesCacheProps } from "../types/types.js";
+import { myCache } from "../app.js";
+import { Product } from "../models/product.js";
 
 export const connectDB = () => {
   mongoose
@@ -8,3 +11,32 @@ export const connectDB = () => {
     .then((c) => console.log(`DB connected to ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
+
+
+//revalidate caching 
+export const invalidadtesCache = async({
+  product,
+  admin,
+  order,
+}: invalidadtesCacheProps)=>{
+     
+  if(product){
+    const productkeys : string [] = [
+      "all-products",
+      "categories",
+     "latest-products"
+    ] ;
+   
+    const product = await Product.find({}).select("_id");
+   
+    product.forEach(element => {
+       productkeys.push(`product-${element._id}`);
+    });
+  
+    myCache.del(productkeys);
+
+  }
+  if(admin){
+
+  }
+}

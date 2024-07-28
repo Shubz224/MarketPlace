6258cf8ca6401820426252844bd42234855bbd07
Shubham Nikam <nikamshubham224@gmail.com>
2024-07-29 00:23:27 +0530

    invalidation for product and  order id added

--- Backend/src/utils/features.ts
+++ Backend/src/utils/features.ts
@@ -1,42 +1,64 @@
 import mongoose from "mongoose";
-import { invalidadtesCacheProps } from "../types/types.js";
+import { invalidadtesCacheProps, OrderItemType } from "../types/types.js";
 import { myCache } from "../app.js";
 import { Product } from "../models/product.js";
+import { Order } from "../models/order.js";
 
-export const connectDB = () => {
+export const connectDB = (uri: string) => {
   mongoose
-    .connect("mongodb://localhost:27017", {
+    .connect(uri, {
       dbName: "store24",
     })
     .then((c) => console.log(`DB connected to ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
 
-
-//revalidate caching 
-export const invalidadtesCache = async({
+//revalidate caching
+export const invalidadtesCache = async ({
   product,
   admin,
   order,
-}: invalidadtesCacheProps)=>{
-     
-  if(product){
-    const productkeys : string [] = [
-      "all-products",
+  userId,
+  orderId,
+  productId
+}: invalidadtesCacheProps) => {
+  //----------------------------------------------------------------------------->product
+  if (product) {
+    const productkeys: string[] = [
+      "latest-products",
       "categories",
-     "latest-products"
-    ] ;
-   
-    const product = await Product.find({}).select("_id");
-   
-    product.forEach(element => {
-       productkeys.push(`product-${element._id}`);
-    });
-  
+      "all-products",
+    ];
+
+    if (typeof productId === "string") productkeys.push(`product-${productId}`);
+
+    if (typeof productId === "object") {
+      productId.forEach((i) => productkeys.push(`product-${i}`));
+    }
+
     myCache.del(productkeys);
-
   }
-  if(admin){
 
+  /////order----------------------------------------->
+  if (order) {
+    const orderKeys: string[] = [
+      "all-orders",
+      `my-orders-${userId}`,
+      `order-${orderId}`,
+    ];
+    myCache.del(orderKeys);
   }
-}
+};
+
+//reduce stock feature which reduces stock after order is placed
+
+export const reduceStock = async (orderItems: OrderItemType[]) => {
+  for (let i = 0; i < orderItems.length; i++) {
+    const order = orderItems[i];
+    const product = await Product.findById(order.productId);
+    if (!product) throw new Error("Product Not Found");
+
+    product.stock -= order.quantity;
+    await product.save();
+  }
+};

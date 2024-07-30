846b1788d48caa180a86c70c7aedd2ba0ac2282b
Shubham Nikam <nikamshubham224@gmail.com>
2024-07-30 08:07:36 +0530

    Payment routes updated

---
+++ Backend/src/routes/payment.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+import { adminOnly } from "../middlewares/auth.js";
+import {
+  allCoupons,
+  applyDiscount,
+  deleteCoupons,
+  newCoupon,
+} from "../controllers/payment.js";
+
+const app = express.Router();
+//api/v1/payment/coupon/new
+app.post("/coupon/new", newCoupon);
+
+//api/v1/payment/discount
+app.get("/discount", applyDiscount);
+
+//api/v1/payment/coupon/all
+app.get("/coupon/all",adminOnly, allCoupons);
+
+//-> /api/payment/coupon/delete:id
+app.delete("/coupon/:id", adminOnly,deleteCoupons);
+
+export default app;

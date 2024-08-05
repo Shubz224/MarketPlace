7826bf8862a4b2fdbf6515ea1c938ef3618713a7
Shubham Nikam <nikamshubham224@gmail.com>
2024-08-05 22:26:07 +0530

    admin features added

--- Backend/src/utils/features.ts
+++ Backend/src/utils/features.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { invalidadtesCacheProps, OrderItemType } from "../types/types.js";
 import { myCache } from "../app.js";
 import { Product } from "../models/product.js";
+import { Document } from "mongoose";
 
 export const connectDB = (uri: string) => {
   mongoose
@@ -13,7 +14,7 @@ export const connectDB = (uri: string) => {
 };
 
 //revalidate caching
-export const invalidadtesCache = async ({
+export const invalidadtesCache = ({
   product,
   admin,
   order,
@@ -47,6 +48,15 @@ export const invalidadtesCache = async ({
     ];
     myCache.del(orderKeys);
   }
+
+  if (admin) {
+    myCache.del([
+      "admin-stats",
+      "admin-pie-charts",
+      "admin-bar-charts",
+      "admin-line-charts",
+    ]);
+  }
 };
 
 //reduce stock feature which reduces stock after order is placed
@@ -62,8 +72,67 @@ export const reduceStock = async (orderItems: OrderItemType[]) => {
   }
 };
 
-export const calculatePercentage = (thisMonth:number, lastMonth: number) => {
-  if (lastMonth === 0) return thisMonth * 100
-  const percent = ((thisMonth - lastMonth) / lastMonth) * 100;
-  return  Number(percent.toFixed(0));
+export const calculatePercentage = (thisMonth: number, lastMonth: number) => {
+  if (lastMonth === 0) return thisMonth * 100;
+  const percent = (thisMonth / lastMonth) * 100;
+  return Number(percent.toFixed(0));
+};
+
+export const getInventries = async ({
+  categories,
+  productsCount,
+}: {
+  categories: string[];
+  productsCount: number;
+}) => {
+  const categoriesCountPromise = categories.map((category) =>
+    Product.countDocuments({ category })
+  );
+
+  const categoriesCount = await Promise.all(categoriesCountPromise);
+
+  const categoryCount: Record<string, number>[] = [];
+
+  categories.forEach((category, i) => {
+    categoryCount.push({
+      [category]: Math.round((categoriesCount[i] / productsCount) * 100),
+    });
+  });
+
+  return categoryCount;
+};
+//----------------------------------------------------------------------------------------------->
+interface MyDocument extends Document {
+  createdAt: Date;
+  discount?: number;
+  total?: number;
+}
+
+type funcProps = {
+  length: number;
+  docArr: MyDocument[];
+  today: Date;
+  property?: "discount" | "total";
+};
+
+//------------------------------------------------------------------------------------------------------>
+
+export const getChartData = ({
+  length,
+  docArr,
+  today,
+  property,
+}: funcProps) => {
+  //const today = new Date();
+  const data: number[] = new Array(length).fill(0);
+
+  docArr.forEach((i) => {
+    const creationDate = i.createdAt;
+    const monthDiff = (today.getMonth() - creationDate.getMonth() + 12) % 12;
+
+    if (monthDiff < length) {
+      data[length - monthDiff - 1] += property ? i[property]! : 1;
+    }
+  });
+  return data;
 };

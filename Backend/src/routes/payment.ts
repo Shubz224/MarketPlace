741fcfc8dee05629eb393dffe1be8aa13e55c492
Shubham Nikam <nikamshubham224@gmail.com>
2024-08-05 22:25:13 +0530

    payment route setup

--- Backend/src/routes/payment.ts
+++ Backend/src/routes/payment.ts
@@ -4,11 +4,15 @@ import { adminOnly } from "../middlewares/auth.js";
 import {
   allCoupons,
   applyDiscount,
+  createPaymentIntent,
   deleteCoupons,
   newCoupon,
 } from "../controllers/payment.js";
 
 const app = express.Router();
+
+app.post("/create", createPaymentIntent);
+
 //api/v1/payment/coupon/new
 app.post("/coupon/new", newCoupon);
 
@@ -16,9 +20,9 @@ app.post("/coupon/new", newCoupon);
 app.get("/discount", applyDiscount);
 
 //api/v1/payment/coupon/all
-app.get("/coupon/all",adminOnly, allCoupons);
+app.get("/coupon/all", adminOnly, allCoupons);
 
 //-> /api/payment/coupon/delete:id
-app.delete("/coupon/:id", adminOnly,deleteCoupons);
+app.delete("/coupon/:id", adminOnly, deleteCoupons);
 
 export default app;

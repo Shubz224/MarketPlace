01eb8e3cfb8eed139b3f61dfefc71fd33f64e0c3
Shubham Nikam <nikamshubham224@gmail.com>
2024-08-20 09:32:35 +0530

    Logout handler

--- frontend/src/Components/header.tsx
+++ frontend/src/Components/header.tsx
@@ -1,63 +1,78 @@
-
-import { Link } from "react-router-dom"
-import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa"
+import { Link } from "react-router-dom";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaUser,
+} from "react-icons/fa";
 import { useState } from "react";
 import { User } from "../types/types";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import toast from "react-hot-toast";
 
 // user added then login page disappeard bug ....
 
-interface PropesType{
-  user:User|null;
+interface PropesType {
+  user: User | null;
 }
 
-const Header = ({user}:PropesType) => {
-
+const Header = ({ user }: PropesType) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-
   //handle logout function
-  const logoutHandler = ()=>{
-    setIsOpen(false);
-  }
-
+  const logoutHandler = async () => {
+    try {
+      await signOut(auth);
+      toast.success("Signout");
+      setIsOpen(false);
+    } catch (error) {
+      toast.error("SignOut Fail");
+    }
+  };
 
   return (
-
     <nav className="header">
-      <Link  onClick={()=>setIsOpen(false)} to={"/"}>HOME</Link>
-      <Link onClick={()=>setIsOpen(false)} to={"/search"}><FaSearch /></Link>
+      <Link onClick={() => setIsOpen(false)} to={"/"}>
+        HOME
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/search"}>
+        <FaSearch />
+      </Link>
 
-      <Link onClick={()=>setIsOpen(false)  } to={"/cart"}><FaShoppingBag /></Link>
-      {
-        user?._id ? (
-          <>
-            <button onClick={() => setIsOpen((prev) => !prev)}>
-              <FaUser />
-            </button>
-            <dialog open={isOpen}>
-              <div>
-                {
-                  user.role === "admin" && (
-                    <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
+      <Link onClick={() => setIsOpen(false)} to={"/cart"}>
+        <FaShoppingBag />
+      </Link>
+      {user?._id ? (
+        <>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+          <dialog open={isOpen}>
+            <div>
+              {user.role === "admin" && (
+                <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
                   Admin
                 </Link>
+              )}
 
-                  )
-                }
-
-                <Link  onClick={()=>setIsOpen(false)} to="/orders">Orders</Link>
-                <button  onClick={ logoutHandler}>
-                  <FaSignOutAlt />
-                </button>
-              </div>
-            </dialog>
-          </>
-        ) : <Link   onClick={()=>setIsOpen(false)}   
-         to={"/login"}><FaSignInAlt />
-         </Link>
-      }
+              <Link onClick={() => setIsOpen(false)} to="/orders">
+                Orders
+              </Link>
+              <button onClick={logoutHandler}>
+                <FaSignOutAlt />
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link onClick={() => setIsOpen(false)} to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
     </nav>
-  )
-}
+  );
+};
 
 export default Header;

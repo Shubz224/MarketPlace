7c59b9110b36c122ef3af6a48677356dc0e5f120
Shubham Nikam <nikamshubham224@gmail.com>
2024-08-21 18:31:34 +0530

    added skeleton for loader

--- frontend/src/Components/loader.tsx
+++ frontend/src/Components/loader.tsx
@@ -4,12 +4,26 @@ const Loader = () => {
 
 export default Loader;
 
-export const Skeleton = ({width = "unset"}:{width:string}) => {
+interface SkeletonProps {
+  width?: string;
+  length?: number;
+  height?: string;
+  containerHeight?: string;
+}
+
+export const Skeleton = ({
+  width = "unset",
+  length = 3,
+  height = "30px",
+  containerHeight = "unset",
+}: SkeletonProps) => {
+  const skeletions = Array.from({ length }, (_, idx) => (
+    <div key={idx} className="skeleton-shape" style={{ height }}></div>
+  ));
+
   return (
-    <div className="skeleton-loader"  style={{width}}>
-      <div className="skeleton-shape"></div>
-      <div className="skeleton-shape"></div>
-      <div className="skeleton-shape"></div>
+    <div className="skeleton-loader" style={{ width, height: containerHeight }}>
+      {skeletions}
     </div>
   );
 };

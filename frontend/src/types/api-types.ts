a5422837ac5bbf7ecd725fe21ef1242f396dc983
Shubham Nikam <nikamshubham224@gmail.com>
2024-08-21 18:33:15 +0530

    Added types for new product and search product for admin features

--- frontend/src/types/api-types.ts
+++ frontend/src/types/api-types.ts
@@ -28,3 +28,23 @@ export type CategoriesResponse = {
   success: boolean;
   categories: string[];
 };
+
+export type SearchProductsResponse = {
+  success: boolean;
+  products: Product[];
+  totalPage: number;
+};
+
+export type SearchProductsRequest = {
+  search: string;
+  price: number;
+  category: string;
+  page: number;
+  sort:string,
+};
+
+
+export type NewProductRequest = {
+ id:string;
+  formData: FormData;
+};

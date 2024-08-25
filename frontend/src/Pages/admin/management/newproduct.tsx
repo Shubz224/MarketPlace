aa535a5ae8d4623eca0f3c91a88b688a6924609c
Shubham Nikam <nikamshubham224@gmail.com>
2024-08-25 14:12:26 +0530

    made restype error (FetchBaseQueryError | SerializedError)optional

--- frontend/src/Pages/admin/management/newproduct.tsx
+++ frontend/src/Pages/admin/management/newproduct.tsx
@@ -1,13 +1,26 @@
-import { ChangeEvent, useState } from "react";
-import AdminSidebar from "../../../components/admin/AdminSidebar";
+import { ChangeEvent, FormEvent, useState } from "react";
+import AdminSidebar from "../../../Components/admin/AdminSidebar";
+import { useSelector } from "react-redux";
+import { userReducerInitialstate } from "../../../types/reducer-types";
+import { useNewProductMutation } from "../../../redux/api/productAPI";
+
+import { useNavigate } from "react-router-dom";
+import { responseToast } from "../../../utils/features";
 
 const NewProduct = () => {
+  const { user } = useSelector(
+    (state: { userReducer: userReducerInitialstate }) => state.userReducer
+  );
+
   const [name, setName] = useState<string>("");
   const [category, setCategory] = useState<string>("");
   const [price, setPrice] = useState<number>(1000);
   const [stock, setStock] = useState<number>(1);
   const [photoPrev, setPhotoPrev] = useState<string>("");
   const [photo, setPhoto] = useState<File>();
+
+  const [NewProduct] = useNewProductMutation();
+  const navigate = useNavigate();
 
   const changeImageHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const file: File | undefined = e.target.files?.[0];
@@ -25,16 +38,35 @@ const NewProduct = () => {
     }
   };
 
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!name || !price || !category || !stock || !photo) return;
+
+    const formData = new FormData();
+
+    formData.set("name", name);
+    formData.set("price", price.toString());
+    formData.set("stock", stock.toString());
+    formData.set("photo", photo);
+    formData.set("category", category.toString());
+
+    const res = await NewProduct({ id: user?._id!, formData });
+
+    responseToast(res, navigate, "/admin/product");
+  };
+
   return (
     <div className="admin-container">
       <AdminSidebar />
       <main className="product-management">
         <article>
-          <form>
+          <form onSubmit={submitHandler}>
             <h2>New Product</h2>
             <div>
               <label>Name</label>
               <input
+                required
                 type="text"
                 placeholder="Name"
                 value={name}
@@ -44,6 +76,7 @@ const NewProduct = () => {
             <div>
               <label>Price</label>
               <input
+                required
                 type="number"
                 placeholder="Price"
                 value={price}
@@ -53,6 +86,7 @@ const NewProduct = () => {
             <div>
               <label>Stock</label>
               <input
+                required
                 type="number"
                 placeholder="Stock"
                 value={stock}
@@ -63,6 +97,7 @@ const NewProduct = () => {
             <div>
               <label>Category</label>
               <input
+                required
                 type="text"
                 placeholder="eg. laptop, camera etc"
                 value={category}
@@ -72,7 +107,8 @@ const NewProduct = () => {
 
             <div>
               <label>Photo</label>
-              <input type="file" onChange={changeImageHandler} />
+
+              <input required type="file" onChange={changeImageHandler} />
             </div>
 
             {photoPrev && <img src={photoPrev} alt="New Image" />}

7fb1e1f38c9bc94e74723c26739eff179bfc3b00
Shubham Nikam <nikamshubham224@gmail.com>
2024-08-25 15:36:45 +0530

    updated response type of new product query

--- frontend/src/redux/api/productAPI.ts
+++ frontend/src/redux/api/productAPI.ts
@@ -4,6 +4,7 @@ import {
   CategoriesResponse,
   MessageResponse,
   NewProductRequest,
+  ProductResponse,
   SearchProductsRequest,
   SearchProductsResponse,
 } from "../../types/api-types";
@@ -13,18 +14,23 @@ export const productAPI = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: `${import.meta.env.VITE_SERVER}/api/v1/product/`,
   }),
+  tagTypes: ["product"],
   endpoints: (builder) => ({
     latestProducts: builder.query<AllProductsResponse, string>({
       query: () => "latest",
+      providesTags: ["product"],
     }),
+
     //---------------------------------------------------------------------------------------------->
     allproducts: builder.query<AllProductsResponse, string>({
       query: (id) => `admin-products?id=${id}`,
+      providesTags: ["product"],
     }),
 
     //-------------------------------------------------------------------------------------->
     categories: builder.query<CategoriesResponse, string>({
       query: () => `categories`,
+      providesTags: ["product"],
     }),
     //---------------------------------------------------------------------------------->
     SearchProducts: builder.query<
@@ -35,10 +41,17 @@ export const productAPI = createApi({
         let base = `all?search= ${search}&page=${page}`;
         if (price) base += `&price=${price}`;
         if (sort) base += `&sort=${sort}`;
-        if (category) base += `&price=${category}`;
+        if (category) base += `&category=${category}`;
         return base;
       },
+      providesTags: ["product"],
     }),
+
+    productdetails: builder.query<ProductResponse, string>({
+      query: (id) => id,
+      providesTags: ["product"],
+    }),
+
     //------------------------------------------------------------------------------------------------------------->
     newProduct: builder.mutation<MessageResponse, NewProductRequest>({
       query: ({ formData, id }) => ({
@@ -46,6 +59,7 @@ export const productAPI = createApi({
         method: "POST",
         body: formData,
       }),
+      invalidatesTags: ["product"],
     }),
   }),
 });
@@ -56,4 +70,5 @@ export const {
   useCategoriesQuery,
   useSearchProductsQuery,
   useNewProductMutation,
+  useProductdetailsQuery,
 } = productAPI;

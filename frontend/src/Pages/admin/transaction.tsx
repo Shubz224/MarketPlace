334e9d4239131dd68296c96c23a041945c6e0ad5
Shubham Nikam <nikamshubham224@gmail.com>
2024-09-22 22:29:56 +0530

    Transaction  management

--- frontend/src/Pages/admin/transaction.tsx
+++ frontend/src/Pages/admin/transaction.tsx
@@ -1,14 +1,14 @@
 import { ReactElement, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Column } from "react-table";
 import AdminSidebar from "../../Components/admin/AdminSidebar";
-import TableHOC from "../..//Components/admin/TableHOC";
-import { useSelector } from "react-redux";
-import { userReducerInitialstate } from "../../types/reducer-types";
-import { useAllOrdersQuery } from "../../redux/api/orderAPI";
-import toast from "react-hot-toast";
-import { CustomError } from "../../types/api-types";
+import TableHOC from "../../Components/admin/TableHOC";
 import { Skeleton } from "../../Components/loader";
+import { useAllOrdersQuery } from "../../redux/api/orderAPI";
+import { RootState } from "../../redux/store";
+import { CustomError } from "../../types/api-types";
 
 interface DataType {
   user: string;
@@ -47,25 +47,23 @@ const columns: Column<DataType>[] = [
 ];
 
 const Transaction = () => {
-  const { user } = useSelector(
-    (state: { userReducer: userReducerInitialstate }) => state.userReducer
-  );
-  
+  const { user } = useSelector((state: RootState) => state.userReducer);
 
-
-  const { isLoading, isError, error, data } = useAllOrdersQuery(user?._id!);
+  const { isLoading, data, isError, error } = useAllOrdersQuery(user?._id!);
 
   const [rows, setRows] = useState<DataType[]>([]);
 
-
-
-  if (isError) toast.error((error as CustomError).data.message);
+  if (isError) {
+    const err = error as CustomError;
+    toast.error(err.data.message);
+  }
 
   useEffect(() => {
-    if (data)
+    if (data) {
+      console.log(data.orders); // Debugging
       setRows(
         data.orders.map((i) => ({
-          user: i.user.name,
+          user: i.user ? i.user.name : "Unknown User", // Handle null user
           amount: i.total,
           discount: i.discount,
           quantity: i.orderItems.length,
@@ -85,6 +83,7 @@ const Transaction = () => {
           action: <Link to={`/admin/transaction/${i._id}`}>Manage</Link>,
         }))
       );
+    }
   }, [data]);
 
   const Table = TableHOC<DataType>(
@@ -97,7 +96,7 @@ const Transaction = () => {
   return (
     <div className="admin-container">
       <AdminSidebar />
-      <main>{isLoading ? <Skeleton width="3vh" /> : Table}</main>
+      <main>{isLoading ? <Skeleton length={20} /> : Table}</main>
     </div>
   );
 };

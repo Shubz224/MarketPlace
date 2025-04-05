4316883f12e801f04e75226213e42a3cebb2620c
Shubham Nikam <nikamshubham224@gmail.com>
2025-04-05 18:57:15 +0530

    d

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDV16lCbMSntBqzZAK8RztDzxzfo4_Poro",
-  authDomain: "ecommerce-d9bc0.firebaseapp.com",
-  projectId: "ecommerce-d9bc0",
-  storageBucket: "ecommerce-d9bc0.firebasestorage.app",
-  messagingSenderId: "982774866698",
-  appId: "1:982774866698:web:7a80db8254c847ade750f3"
+//enter yr keys
 };
 
 // Initialize Firebase
